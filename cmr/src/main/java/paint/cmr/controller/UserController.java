942c277dcaa595e725e65da3a19ec35da7433299
Ziemowit <z.piet@10g.pl>
2023-06-12 18:28:09 +0200

    bez https

--- cmr/src/main/java/paint/cmr/controller/UserController.java
+++ cmr/src/main/java/paint/cmr/controller/UserController.java
@@ -2,9 +2,8 @@ package paint.cmr.controller;
 
 
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import paint.cmr.model.User;
 import paint.cmr.service.UserService;
 
 @RestController
@@ -15,8 +14,16 @@ public class UserController {
     public UserController(UserService userService) {
         this.userService = userService;
     }
-    @GetMapping
+    @GetMapping("/users")
     public String allUsers(){
         return userService.allUsers().toString();
+    }
+    @PostMapping("/users")
+    public User addUser(@RequestBody User user){
+        return userService.addUser(user);
+    }
+    @DeleteMapping("/users/{id}")
+    public void delete(@PathVariable String id){
+        userService.delete(id);
     }
 }
--- cmr/src/main/java/paint/cmr/service/UserService.java
+++ cmr/src/main/java/paint/cmr/service/UserService.java
@@ -3,6 +3,7 @@ package paint.cmr.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
 import paint.cmr.model.User;
 import paint.cmr.repository.UserRepository;
 
@@ -19,4 +20,13 @@ public class UserService{
     public List<User> allUsers(){
         return userRepository.findAll();
     }
+
+    public User addUser(@RequestBody User user){
+        return userRepository.save(user);
+    }
+
+    public void delete(String id) {
+        userRepository.deleteById(id);
+    }
+
 }
