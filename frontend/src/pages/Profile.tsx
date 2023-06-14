5a992dff16c06e17c8f412a6df3004de4e499cc3
d0m1n1k4 <125489665+d0m1n1k4@users.noreply.github.com>
2023-06-14 17:40:04 +0200

    Update Profile.tsx  - New action: Delete user

--- frontend/src/pages/Profile.tsx
+++ frontend/src/pages/Profile.tsx
@@ -1,205 +1,91 @@
 import { useState } from 'react';
-import { createStyles, Avatar, Text, Group, Button, Modal, TextInput } from '@mantine/core';
-import { IconPhoneCall, IconAt, IconEdit, IconTrash } from '@tabler/icons-react';
+import { Table, Input, Button } from '@mantine/core';
 
-const useStyles = createStyles((theme) => ({
-  icon: {
-    color: theme.colorScheme === 'dark' ? theme.colors.dark[3] : theme.colors.gray[5],
+const users = [
+  {
+    number: 1,
+    name: 'Małgorzata',
+    surname: 'Pytka',
+    role: 'Frontend Developer'
   },
-  name: {
-    fontFamily: `Greycliff CF, ${theme.fontFamily}`,
+  {
+    number: 2,
+    name: 'Anna',
+    surname: 'Kowalska',
+    role: 'UI Designer'
   },
-}));
+  {
+    number: 3,
+    name: 'Jan',
+    surname: 'Nowakowski',
+    role: 'Backend Developer'
+  },
+  {
+    number: 4,
+    name: 'Maria',
+    surname: 'Lewandowska',
+    role: 'DB Administrator'
+  },
+];
 
-interface UserInfoIconsProps {
-  avatar: string;
-  name: string;
-  title: string;
-  phone: string;
-  email: string;
-  onSaveChanges: (name: string, title: string, phone: string, email: string) => void;
-  onDeleteAccount: () => void;
-}
+export default function Users() {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [filteredUsers, setFilteredUsers] = useState(users);
 
-export function UserInfoIcons({
-  avatar,
-  name,
-  title,
-  phone,
-  email,
-  onSaveChanges,
-  onDeleteAccount,
-}: UserInfoIconsProps) {
-  const { classes } = useStyles();
-  const [isEditing, setIsEditing] = useState(false);
-  const [editedName, setEditedName] = useState(name);
-  const [editedTitle, setEditedTitle] = useState(title);
-  const [editedPhone, setEditedPhone] = useState(phone);
-  const [editedEmail, setEditedEmail] = useState(email);
-  const [showDeleteConfirmation, setShowDeleteConfirmation] = useState(false);
-
-  const openEditModal = () => {
-    setIsEditing(true);
+  const handleDeleteUser = (number) => {
+    const updatedUsers = filteredUsers.filter((user) => user.number !== number);
+    setFilteredUsers(updatedUsers);
   };
 
-  const closeEditModal = () => {
-    setIsEditing(false);
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
+    const filteredUsers = users.filter((user) => {
+      const fullName = `${user.name} ${user.surname}`;
+      return fullName.toLowerCase().includes(event.target.value.toLowerCase());
+    });
+    setFilteredUsers(filteredUsers);
   };
 
-  const saveChanges = () => {
-    onSaveChanges(editedName, editedTitle, editedPhone, editedEmail);
-    closeEditModal();
-  };
-
-  const confirmDeleteAccount = () => {
-    setShowDeleteConfirmation(true);
-  };
-
-  const handleDeleteConfirmation = (confirmed: boolean) => {
-    setShowDeleteConfirmation(false);
-    if (confirmed) {
-      onDeleteAccount();
-    }
-  };
-
-  return (
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '50vh' }}>
-      <Group noWrap>
-        <Avatar src={avatar} size={250} radius="50%" />
-        <div>
-          <Text fz="lg" tt="uppercase" fw={700} c="dimmed">
-            {title}
-          </Text>
-
-          <Text fz="lg" fw={500} className={classes.name}>
-            {name}
-          </Text>
-
-          <Group noWrap spacing={10} mt={10}>
-            <IconAt stroke={1.5} size="1.5rem" className={classes.icon} />
-            <Text fz="xf" c="dimmed">
-              {email}
-            </Text>
-          </Group>
-
-          <Group noWrap spacing={10} mt={10}>
-            <IconPhoneCall stroke={1.5} size="1.5rem" className={classes.icon} />
-            <Text fz="xf" c="dimmed">
-              {phone}
-            </Text>
-          </Group>
-
-          <div style={{ marginTop: '16px', display: 'flex', alignItems: 'center' }}>
-            <Button
-              variant="outline"
-              leftIcon={<IconEdit />}
-              onClick={openEditModal}
-              size="sm"
-              style={{ marginRight: '8px' }}
-            >
-              Edit
-            </Button>
-            <Button
-              variant="outline"
-              leftIcon={<IconTrash />}
-              onClick={confirmDeleteAccount}
-              size="sm"
-              style={{ marginRight: '8px' }}
-            >
-              Delete
-            </Button>
-          </div>
-
-          <Modal
-            opened={isEditing}
-            onClose={closeEditModal}
-            title="Edit your data"
-            size="xs"
-            withCloseButton
-          >
-            <div style={{ display: 'flex', flexDirection: 'column' }}>
-              <TextInput
-                label="Name"
-                value={editedName}
-                onChange={(event) => setEditedName(event.currentTarget.value)}
-                required
-                style={{ marginBottom: '16px' }}
-              />
-              <TextInput
-                label="Title"
-                value={editedTitle}
-                onChange={(event) => setEditedTitle(event.currentTarget.value)}
-                required
-                style={{ marginBottom: '16px' }}
-              />
-              <TextInput
-                label="Phone"
-                value={editedPhone}
-                onChange={(event) => setEditedPhone(event.currentTarget.value)}
-                required
-                style={{ marginBottom: '16px' }}
-              />
-              <TextInput
-                label="Email"
-                value={editedEmail}
-                onChange={(event) => setEditedEmail(event.currentTarget.value)}
-                required
-                style={{ marginBottom: '16px' }}
-              />
-              <Button onClick={saveChanges}>Save</Button>
-            </div>
-          </Modal>
-
-          <Modal
-            opened={showDeleteConfirmation}
-            onClose={() => handleDeleteConfirmation(false)}
-            title="Are you sure you want to delete your account?"
-            size="xs"
-            withCloseButton
-          >
-            <Button onClick={() => handleDeleteConfirmation(true)} color="red" variant="outline">
-              Yes
-            </Button>
-            <Button onClick={() => handleDeleteConfirmation(false)}variant="outline"> 
-              No
-            </Button>
-          </Modal>
-        </div>
-      </Group>
-    </div>
+  const ths = (
+    <tr>
+      <th>Lp.</th>
+      <th>Imię</th>
+      <th>Nazwisko</th>
+      <th>Rola</th>
+      <th>Akcje</th>
+    </tr>
   );
-}
 
-export default function Profile() {
-  const [name, setName] = useState('Malgosia Pytka');
-  const [title, setTitle] = useState('Frontend Developer');
-  const [phone, setPhone] = useState('123456789');
-  const [email, setEmail] = useState('malgosia.pytka@gmail.com');
-  const [avatar, setAvatar] = useState('https://as2.ftcdn.net/v2/jpg/02/13/52/07/1000_F_213520743_0UqSd3TUYdBK7ep7p7YT1zTX7oUf86Pz.jpg');
-
-  const onSaveChanges = (editedName: string, editedTitle: string, editedPhone: string, editedEmail: string) => {
-    setName(editedName);
-    setTitle(editedTitle);
-    setPhone(editedPhone);
-    setEmail(editedEmail);
-  };
-
-  const onDeleteAccount = () => {
-    
-  };
+  const rows = filteredUsers.map((user) => (
+    <tr key={user.number}>
+      <td>{user.number}</td>
+      <td>{user.name}</td>
+      <td>{user.surname}</td>
+      <td>{user.role}</td>
+      <td>
+        <Button
+          onClick={() => handleDeleteUser(user.number)}
+          variant="outline"
+          color="red"
+          size="xs"
+        >
+          Usuń
+        </Button>
+      </td>
+    </tr>
+  ));
 
   return (
     <>
-      <h1>Profile</h1>
-      <UserInfoIcons
-        avatar={avatar}
-        name={name}
-        title={title}
-        phone={phone}
-        email={email}
-        onSaveChanges={onSaveChanges}
-        onDeleteAccount={onDeleteAccount}
+      <Input
+        placeholder="Wyszukaj użytkownika"
+        value={searchTerm}
+        onChange={handleSearch}
       />
+      <Table>
+        <thead>{ths}</thead>
+        <tbody>{rows}</tbody>
+      </Table>
     </>
   );
 }
